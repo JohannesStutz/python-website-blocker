3428c3ac629326017bc7c2f943edfd0726e67422
Johannes Stutz <61063548+JohannesStutz@users.noreply.github.com>
2021-01-30 17:42:39 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# python-website-blocker
- Focus on your work by blocking your favorite productivity killers.
+# Python Website Blocker
+Focus on your work by blocking your favorite productivity killers.
+
+## Features
+This Python script blocks certain websites from being accessed. You can customize the URLs you want to block by editing `blocklist.txt`.
+
+## How to install
+**The Python Website Blocker currently only works for Windows.**
+Download `blocker.py` and `blocklist.txt` and place them in the same directory.
+You can `pip install win10toast` for nice Windows notifications. But you don't have to, notifications are also printed in your command line tool.
+
+## How to run
+To run the block script, **you have to run your command line tool with administrator privileges.**
+To block websites:
+```
+cd /your/directory/
+python blocker.py
+```
+To unblock all websites:
+```
+python blocker.py unblock
+```
+
+## Behind the scenes
+The script blocks URLs by modifying the `hosts` file. Blocked URLs are redirected to `127.0.0.1`. The script backs up the original `hosts` file, you will not lose any customizations and you can always reset to the original state.
+
+## To Do
+- Turn script into installable Python package
+- extend to Linux and MacOS
+- Add scheduling, custom times or Pomodoro timer
---
+++ blocker.py
@@ -0,0 +1,85 @@
+from datetime import datetime as dt
+from pathlib import Path
+from shutil import copy
+import sys
+try:
+	from win10toast import ToastNotifier
+	toaster = ToastNotifier()
+except:
+	toaster = False
+
+APP_NAME = 'Website Blocker'
+REDIRECT = '127.0.0.1'
+WIN_PATH = r'C:\Windows\System32\drivers\etc'
+
+host_path = Path(WIN_PATH)
+host_file = host_path/'hosts'
+host_file_copy = host_path/'hosts.original'
+host_file_blocked = host_path/'hosts.blocked'
+
+class Blocker():
+	def __init__(self, redirect=REDIRECT):
+		self.redirect = redirect
+		with open('blocklist.txt') as file:
+			self.blocked = [line.rstrip() for line in file]
+		if not host_file_copy.exists(): self._setup()
+		self._create_blocked_list()
+
+	def _setup(self):
+		# Create copy of hosts file
+		try:
+			copy(host_file, host_file_copy)
+			print("Copy of hosts file created")
+		except PermissionError:
+			self._raise_permission_error()
+
+	def _create_blocked_list(self):
+		try:
+			copy(host_file_copy, host_file_blocked)
+			with open(host_file_blocked, "a") as blocked_file:
+				for url in self.blocked:
+					# TODO: refine, add www only if not in url
+					blocked_file.write(f"{self.redirect} {url}\n")
+					blocked_file.write(f"{self.redirect} www.{url}\n")
+				print("hosts.blocked created")
+		except PermissionError:
+			self._raise_permission_error()
+
+	def block(self):
+		try:
+			copy(host_file_blocked, host_file)
+			print("Hosts file replaced with hosts.blocked")
+			return True
+		except PermissionError:
+			self._raise_permission_error()
+			return False
+
+	def unblock(self):
+		try:
+			copy(host_file_copy, host_file)
+			print("Hosts file replaced with hosts.original")
+			return True
+		except PermissionError:
+			self._raise_permission_error()
+			return False
+
+	def notify(self, message, title=APP_NAME):
+		print(message)
+		if toaster:
+			toaster.show_toast(title, message, duration=5)
+
+	def _raise_permission_error(self):
+		self.notify("Permission Error. Please run the command line tool as ADMINISTRATOR.")
+
+if __name__=='__main__':
+	blocker = Blocker()
+	try: mode = sys.argv[1]
+	except: mode = 'block' 
+
+	if mode == 'unblock':
+		if blocker.unblock():
+			blocker.notify("Websites unblocked, have fun")
+	else:
+		if blocker.block():
+			blocker.notify("Websites blocked, enjoy your work")
+
